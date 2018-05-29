db9ea8620a38c387bec63f9a436882f0538406df
Norman Riobo <nriobo@gmail.com>
2018-05-29 20:15:24 +0200

    Added class to fetch weather information and InstrumentedTest
    - added class to fetch information from openweathermap API
    - added instrumented test for that class

--- app/src/androidTest/java/com/nriobo/voicerecognition/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nriobo/voicerecognition/ExampleInstrumentedTest.java
@@ -3,7 +3,11 @@ package com.nriobo.voicerecognition;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +20,31 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private Context appContext;
+
+    @Before
+    public void setup() {
+        appContext = InstrumentationRegistry.getTargetContext();
+    }
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.nriobo.voicerecognition", appContext.getPackageName());
+       assertEquals("com.nriobo.voicerecognition", appContext.getPackageName());
     }
+
+    // to check if the connection to API was well implemented
+    @Test
+    public void fetchWeatherInfo_codIs200() {
+        String cod= "0";
+        JSONObject json = new JSONObject();
+        try {
+            json = FetchWeatherInfo.getJSON(appContext, "Hamburg");
+            cod = json.getString("cod");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("200", cod);
+    }
+
 }
